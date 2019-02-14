e94697c040bdfa8b4c78e5ebb27ba07a23a539c7
Ramon Fernandes <ramonfernandes9@gmail.com>
2019-02-14 16:29:48 -0200

    Inserindo uma nova categoria com POST

--- src/main/java/com/ramon/cursomc/services/CategoriaService.java
+++ src/main/java/com/ramon/cursomc/services/CategoriaService.java
@@ -17,9 +17,14 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-                          "Objeto não encontrado! id " + id + ", Tipo: " + Categoria.class.getName()));
-		
+                          "Objeto não encontrado! id " + id + ", Tipo: " + Categoria.class.getName()));	
 	}	
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);		
+	}
+	
+	
 }
