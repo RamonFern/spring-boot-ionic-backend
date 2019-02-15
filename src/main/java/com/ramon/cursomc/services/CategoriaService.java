c688940d2046d0b65fca4c4726fc06d30332b9ad
Ramon Fernandes <ramonfernandes9@gmail.com>
2019-02-14 22:51:01 -0200

    Deletando Categoria e Exceptions

--- src/main/java/com/ramon/cursomc/services/CategoriaService.java
+++ src/main/java/com/ramon/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.ramon.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ramon.cursomc.domain.Categoria;
 import com.ramon.cursomc.repositories.CategoriaRepository;
+import com.ramon.cursomc.services.exceptions.DataIntegrityExceptions;
 import com.ramon.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);		
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityExceptions("Não é possível excluir uma Categoria que possui produtos.");
+		}
 	}
 	
 	
