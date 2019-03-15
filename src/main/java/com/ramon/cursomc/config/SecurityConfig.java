ed57a42e9e51a7076fa8d25166f78e62bbdda23f
Ramon Fernandes <ramonfernandes9@gmail.com>
2019-03-15 13:19:29 -0300

    Implementando autenticação e geração do token JWT/tive que comentar um código da aula para poder funcionar

--- src/main/java/com/ramon/cursomc/config/SecurityConfig.java
+++ src/main/java/com/ramon/cursomc/config/SecurityConfig.java
@@ -5,22 +5,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.ramon.cursomc.security.JWTAuthenticationFilter;
+import com.ramon.cursomc.security.JWTUtil;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends  WebSecurityConfigurerAdapter{
-	
+	/*obs.: tive que comentar para funcionar na aula 68 e 69
+	@Autowired
+	private UserDetailsService userDetailsService;
+	*/
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
@@ -44,11 +55,17 @@ public class SecurityConfig extends  WebSecurityConfigurerAdapter{
 		    .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		    .antMatchers(PUBLIC_MATCHERS).permitAll()
 		    .anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
+	/* obs.: tive que comentar para funcionar na aula 68 e 69
+	@Bean
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+	    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}*/
 	
 	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
+	public CorsConfigurationSource corsConfigurationSource() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
