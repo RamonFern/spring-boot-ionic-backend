13f0c6f4c6752873c501f1ebf4f509fd2dd04b73
Ramon Fernandes <ramonfernandes9@gmail.com>
2019-03-16 00:25:31 -0300

    Esqueci a Senha

--- src/main/java/com/ramon/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/ramon/cursomc/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.ramon.cursomc.domain.Cliente;
 import com.ramon.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService{
@@ -70,6 +71,22 @@ public abstract class AbstractEmailService implements EmailService{
 		mmh.setText(htmlFromTemplatePedido(obj));
 		return mimeMessage;
 		
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = preparedNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage preparedNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha!");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: "+ newPass);
+		return sm;
 	}
 }
 
--- src/main/java/com/ramon/cursomc/services/EmailService.java
+++ src/main/java/com/ramon/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.ramon.cursomc.domain.Cliente;
 import com.ramon.cursomc.domain.Pedido;
 public interface EmailService {
 
@@ -13,5 +14,7 @@ public interface EmailService {
 	
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
-	void sendHtmlEmail(MimeMessage msg); 
+	void sendHtmlEmail(MimeMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass); 
 }
