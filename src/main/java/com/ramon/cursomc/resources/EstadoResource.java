36dae5b569ea4a6357e0f4e834f0a130b084b0b9
Ramon Fernandes <ramonfernandes9@gmail.com>
2019-04-13 17:51:37 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/ramon/cursomc/resources/EstadoResource.java
@@ -0,0 +1,46 @@
+package com.ramon.cursomc.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ramon.cursomc.domain.Cidade;
+import com.ramon.cursomc.domain.Estado;
+import com.ramon.cursomc.dto.CidadeDTO;
+import com.ramon.cursomc.dto.EstadoDTO;
+import com.ramon.cursomc.services.CidadeService;
+import com.ramon.cursomc.services.EstadoService;
+
+@RestController
+@RequestMapping(value="/estados")
+public class EstadoResource {
+	
+	@Autowired
+	private EstadoService service;
+	
+	@Autowired
+	private CidadeService cidadeService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<EstadoDTO>> findAll(){
+		List<Estado> list = service.findAll();
+		List<EstadoDTO> listDTO = list.stream().map(obj -> new EstadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value="/{estadoId}/cidades", method=RequestMethod.GET)
+	public ResponseEntity<List<CidadeDTO>> findCidades(@PathVariable Integer estadoId){
+		List<Cidade> list = cidadeService.findByEstado(estadoId);
+		List<CidadeDTO> listDto = list.stream().map(obj -> new CidadeDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+ 		
+	}
+	
+
+}
---
+++ src/main/java/com/ramon/cursomc/services/CidadeService.java
@@ -0,0 +1,21 @@
+package com.ramon.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramon.cursomc.domain.Cidade;
+import com.ramon.cursomc.repositories.CidadeRepository;
+
+@Service
+public class CidadeService {
+	
+	@Autowired
+	private CidadeRepository repo;
+	
+	public List<Cidade> findByEstado(Integer estadoId){
+		return repo.findCidade(estadoId);
+	}
+
+}
